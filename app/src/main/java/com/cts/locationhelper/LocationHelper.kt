d628a8144b5e89737ccd0ad957a5a5b4b6c61f12
amnalqahtani <amnalqahtani@elm.sa>
2021-12-24 16:11:04 +0300

    last time runnable after downgrading gradle and removing dependencies

--- app/src/main/java/com/cts/locationhelper/LocationHelper.kt
+++ app/src/main/java/com/cts/locationhelper/LocationHelper.kt
@@ -18,7 +18,7 @@ import kotlin.coroutines.suspendCoroutine
 
 class LocationHelper(private val context: Context, private val intervalMillis : Long = 3000) : CoroutineScope {
 
-    inline fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
+    private fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
         return if (p1 != null && p2 != null) block(p1, p2) else null
     }
 
@@ -47,10 +47,12 @@ class LocationHelper(private val context: Context, private val intervalMillis : 
             withContext(Dispatchers.Main) {
                 location?.let {
                     onSuccess(it)
-                } ?: onError(ERROR_LOCATION_NULL)
+                } ?: onError(ERROR_LOCATION_NO_GPS_AVAILABLE)
             }
         } else {
-            onError(ERROR_LOCATION_PERMISSION)
+            withContext(Dispatchers.Main){
+                onError(ERROR_LOCATION_PERMISSION)
+            }
         }
     }
 
@@ -66,6 +68,7 @@ class LocationHelper(private val context: Context, private val intervalMillis : 
         val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(locationResult: LocationResult) {
+                Log.d("???", "location provider -> ${locationResult.lastLocation.provider}")
                 Log.d("????", "outside accuracy location received: ${locationResult.lastLocation}")
                 val lastLocation = locationResult.lastLocation
                 lastLocation?.let { location ->
@@ -118,7 +121,7 @@ class LocationHelper(private val context: Context, private val intervalMillis : 
 
     companion object {
         const val ERROR_LOCATION_PERMISSION = 1
-        const val ERROR_LOCATION_NULL = 2
+        const val ERROR_LOCATION_NO_GPS_AVAILABLE = 2
     }
 
 }
